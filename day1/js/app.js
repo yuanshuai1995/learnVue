50db314f646e774ea4211fd1b6a63b3cb9fb1918
yuanshuai1995 <369271498@qq.com>
2019-03-26 13:05:13 +0800

    update by app1-app6

--- day1/js/app.js
+++ day1/js/app.js
@@ -1,6 +1,50 @@
 var app = new Vue({
-    el: "#app",
-    data: {
-        message: "Hello"
-    }
+	el: "#app",
+	data: {
+		message: "Hello"
+	}
+});
+var app2 = new Vue({
+	el: "#app2",
+	data: {
+		message: "页面加载于" + new Date().toLocaleString()
+	}
+})
+var app3 = new Vue({
+	el: "#app3",
+	data: {
+		seen: true
+	}
+})
+var app4 = new Vue({
+	el: "#app4",
+	data: {
+		todos: [{
+				text: "学习js"
+			},
+			{
+				text: "学习vue"
+			},
+			{
+				text: "学习整个项目"
+			}
+		]
+	}
+})
+var app5 = new Vue({
+	el: "#app5",
+	data: {
+		message: "abc def g"
+	},
+	methods: {
+		reverseMessage: function() {
+			this.message = this.message.split("").reverse().join("")
+		}
+	}
+})
+var app6 = new Vue({
+	el: "#app6",
+	data: {
+		message: "Hello Vue!"
+	}
 })
